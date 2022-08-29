3acd34fbc56b2ead943bb8124a579fb455a73b07
BoBoCo <53622133+whaijungit@users.noreply.github.com>
2022-08-29 15:42:00 +0800

    unknow

--- src/modules/base.ts
+++ src/modules/base.ts
@@ -1,5 +1,5 @@
 const ESTYPE = 'number'| 'string' | 'array' | 'object' | 'symbol' | 'bigint' | 'undefined' | 'boolean'
 
 const t = (o:any)=>{
-  return Object.prototype.toString.call(o).split(' ')[1].replace(/\]/,'').toLocaleLowerCase() as 
+  return Object.prototype.toString.call(o).split(' ')[1].replace(/\]/,'').toLocaleLowerCase() as unknown as ESTYPE
 }
