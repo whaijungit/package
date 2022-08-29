21fcf86e68f9ab08de5e16aa8e0e670a94956344
BoBoCo <53622133+whaijungit@users.noreply.github.com>
2022-08-29 15:40:56 +0800

    Create base.ts

---
+++ src/modules/base.ts
@@ -0,0 +1,5 @@
+const ESTYPE = 'number'| 'string' | 'array' | 'object' | 'symbol' | 'bigint' | 'undefined' | 'boolean'
+
+const t = (o:any)=>{
+  return Object.prototype.toString.call(o).split(' ')[1].replace(/\]/,'').toLocaleLowerCase() as 
+}
